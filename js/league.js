cb68f5341a28f2f644ce90132f0fcd8fc43e3c5c
Richard Nadji <richardnadji@hotmail.com>
2017-06-08 01:34:24 +0200

    add function constructor League

---
+++ js/league.js
@@ -0,0 +1,9 @@
+function League(title, year, teams) {
+  this.title = title;
+  this.year = year;
+  this.teams = teams;
+}
+
+League.prototype.hasEnded = function() {
+  // if all teams played a match against each other in the same division
+}
